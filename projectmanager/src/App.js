c6a4ed237827423b7d88c430e65b0cd0eda747e6
subash karki <karki011@gmail.com>
2018-04-30 20:57:18 -0500

    skeleton setup

--- projectmanager/src/App.js
+++ projectmanager/src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
 import './App.css';
-
 class App extends Component {
+  constructor (){
+    super();
+    this.state = {
+      projects: [
+        {
+          title: "Business Development",
+          Description: "Web Design"
+        },
+        {
+          title: "Social Media",
+          Description: "Marketing"
+        },
+        {
+          title: "Shoping Cart",
+          Description: "Plan and subcription"
+        }
+      ]
+    }
+  }
+
+  componentWillMount(){
+    
+  }
   render() {
     return (
       <div className="App">
-        This is new
 
-        <Projects/>
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
